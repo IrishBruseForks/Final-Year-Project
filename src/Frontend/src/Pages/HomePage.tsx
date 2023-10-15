d0a14fc8e01ce21ba5aa795d839a22c82de7bb31
IrishBruse <Econn50@outlook.com>
2023-10-16 00:48:56 +0100

    Add mobile breakpoints and app bar

--- src/Frontend/src/Pages/HomePage.tsx
+++ src/Frontend/src/Pages/HomePage.tsx
@@ -1,16 +1,106 @@
-import { Box, Paper, Stack } from "@mui/material";
+import { AccountCircle, Menu as MenuIcon } from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Paper,
+  Stack,
+  SwipeableDrawer,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 import Grid from "@mui/material/Grid";
+import { useState } from "react";
 
 function HomePage() {
+  const [opened, setOpened] = useState(false);
+  const isMobile = useMediaQuery("(max-width:900px)");
+
   return (
     <Box sx={{ height: "100vh" }}>
+      <AppBar position="static">
+        <Toolbar>
+          {isMobile && (
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          )}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Photos
+          </Typography>
+          {true && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                // onClick={}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                // anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(false)}
+                // onClose={handleClose}
+              >
+                <MenuItem
+                // onClick={handleClose}
+                >
+                  Profile
+                </MenuItem>
+                <MenuItem
+                // onClick={handleClose}
+                >
+                  My account
+                </MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
       <Grid container columnSpacing={2} sx={{ height: "100%" }}>
-        <Grid item xs={12} md={3}>
-          <Stack spacing={2} sx={{ height: "100%", pb: { xs: 2, md: 0 } }}>
-            <Paper sx={{ height: "100%" }}>1</Paper>
-            <Paper sx={{ height: "100%" }}>2</Paper>
-          </Stack>
-        </Grid>
+        {isMobile ? (
+          <SwipeableDrawer
+            open={opened}
+            onOpen={() => setOpened(true)}
+            onClose={() => setOpened(false)}
+          >
+            <Grid item xs={12} md={3}>
+              <Stack spacing={2} sx={{ height: "100%", pb: { xs: 2, md: 0 } }}>
+                <Paper sx={{ height: "100%" }}>1</Paper>
+                <Paper sx={{ height: "100%" }}>2</Paper>
+              </Stack>
+            </Grid>
+          </SwipeableDrawer>
+        ) : (
+          <Grid item xs={12} md={3}>
+            <Stack spacing={2} sx={{ height: "100%", pb: { xs: 2, md: 0 } }}>
+              <Paper sx={{ height: "100%" }}>1</Paper>
+              <Paper sx={{ height: "100%" }}>2</Paper>
+            </Stack>
+          </Grid>
+        )}
         <Grid item xs={12} md={9}>
           <Stack spacing={2} sx={{ height: "100%" }}>
             <Paper sx={{ height: "100%" }}>3</Paper>
