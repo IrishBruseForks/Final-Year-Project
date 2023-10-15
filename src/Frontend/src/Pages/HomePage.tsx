ac10094a3db28b4e0a59eddf85871830c85cc23b
IrishBruse <Econn50@outlook.com>
2023-10-16 00:09:08 +0100

    Layout the home page

--- src/Frontend/src/Pages/HomePage.tsx
+++ src/Frontend/src/Pages/HomePage.tsx
@@ -1,97 +1,25 @@
-import Box from "@mui/material/Box";
+import { Box, Paper, Stack } from "@mui/material";
 import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 
-function MyBoxes() {
+function HomePage() {
   return (
-    <Grid container spacing={3} style={{ height: "100vh", padding: "15px" }}>
-      {/* Left Side */}
-      <Grid item xs={12} sm={3}>
-        <Box
-          sx={{
-            backgroundColor: "#18191A",
-            height: "30%",
-            margin: "10px",
-            borderRadius: 2,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Typography variant="h5">
-            Profile and Settings Box (login/logout - report bugs etc)
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            backgroundColor: "#18191A",
-            height: "70%",
-            margin: "10px",
-            borderRadius: 2,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Typography variant="h6">
-            NAVIGATION: List of Chats, Friends, Notifications etc...
-          </Typography>
-        </Box>
+    <Box sx={{ height: "100vh" }}>
+      <Grid container columnSpacing={2} sx={{ height: "100%" }}>
+        <Grid item xs={12} md={3}>
+          <Stack spacing={2} sx={{ height: "100%", pb: { xs: 2, md: 0 } }}>
+            <Paper sx={{ height: "100%" }}>1</Paper>
+            <Paper sx={{ height: "100%" }}>2</Paper>
+          </Stack>
+        </Grid>
+        <Grid item xs={12} md={9}>
+          <Stack spacing={2} sx={{ height: "100%" }}>
+            <Paper sx={{ height: "100%" }}>3</Paper>
+            <Paper sx={{ height: "100%" }}>4</Paper>
+          </Stack>
+        </Grid>
       </Grid>
-
-      {/* Middle */}
-      <Grid item xs={12} sm={6}>
-        <Box
-          sx={{
-            backgroundColor: "#18191A",
-            height: "80%",
-            margin: "10px",
-            borderRadius: 2,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Typography variant="h6">
-            Before clicking on a chat will be a logo or a welcome back *name
-            here*. Then it will display text and group info etc...{" "}
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            backgroundColor: "#18191A",
-            height: "20%",
-            margin: "10px",
-            borderRadius: 2,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Typography variant="h6">
-            Keyboard or buttons for AI or other multimedia functions.
-          </Typography>
-        </Box>
-      </Grid>
-
-      {/* Right Side */}
-      <Grid item xs={12} sm={3}>
-        <Box
-          sx={{
-            backgroundColor: "#18191A",
-            height: "100%",
-            margin: "10px",
-            borderRadius: 2,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Typography variant="h6">More features here.</Typography>
-        </Box>
-      </Grid>
-    </Grid>
+    </Box>
   );
 }
 
-export default MyBoxes;
+export default HomePage;
