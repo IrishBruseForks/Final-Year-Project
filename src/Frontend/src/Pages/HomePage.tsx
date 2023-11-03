d664773e89b1a8ab452f897dc8cd0aba65b6489a
IrishBruse <Econn50@outlook.com>
2023-11-03 00:49:18 +0000

    Change app name

--- src/Frontend/src/Pages/HomePage.tsx
+++ src/Frontend/src/Pages/HomePage.tsx
@@ -39,7 +39,7 @@ function HomePage() {
             </IconButton>
           )}
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Photos
+            Chatalyst
           </Typography>
           <div>
             <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" color="inherit">
