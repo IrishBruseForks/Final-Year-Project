65a11062e0fb5e838f0f4af5ef13b0d7bf452a20
IrishBruse <Econn50@outlook.com>
2023-11-22 10:18:53 +0000

    update reset script

--- src/Backend/sql/reset.sql
+++ src/Backend/sql/reset.sql
@@ -1,5 +1,5 @@
-drop database railway;
-create database railway;
-use railway;
+drop database b291ptgjfdbymuwd8omh;
+create database b291ptgjfdbymuwd8omh;
+use b291ptgjfdbymuwd8omh;
 source A:\Final-Year-Project\src\Backend\sql\database.sql
 source A:\Final-Year-Project\src\Backend\sql\init.sql
