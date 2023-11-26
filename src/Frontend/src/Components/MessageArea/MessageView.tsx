cbfa8ed544d581a03f2903feb9dde9027ace8382
IrishBruse <Econn50@outlook.com>
2023-11-26 22:48:55 +0000

    Add padding to right

--- src/Frontend/src/Components/MessageArea/MessageView.tsx
+++ src/Frontend/src/Components/MessageArea/MessageView.tsx
@@ -2,7 +2,7 @@ import { Grid, Paper, Stack } from "@mui/material";
 
 function MessageView() {
   return (
-    <Grid item xs={12} md={9}>
+    <Grid item xs={12} md={9} pr={2}>
       <Stack spacing={2} sx={{ height: "100%" }}>
         <Paper sx={{ height: "100%" }}></Paper>
         <Paper sx={{ height: "10%" }}></Paper>
