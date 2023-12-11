e871995326c2a634636cb09bb9c057ff56a2eac6
Ryan Harte <94619708+The-Mad-Ryanosaurus@users.noreply.github.com>
2023-12-11 13:02:36 +0000

    MessageList

--- src/Frontend/src/Components/MessageArea/MessageList.tsx
+++ src/Frontend/src/Components/MessageArea/MessageList.tsx
@@ -1,12 +1,22 @@
 import {} from "@mui/material";
 import { useParams } from "react-router-dom";
 import React, { useState, useEffect } from "react";
+import { useQuery } from "react-query";
+import { ChannelResponse } from "../../Types/ServerTypes";
+import API from "../../Utility/Api";
 
 function MessageList() {
   const { uuid } = useParams<{ uuid: string }>();
+  const { data, isLoading } = useQuery<ChannelResponse[]>("getChannels", () => API.GetChannels());
+  const [channel, setChannel] = useState<ChannelResponse | undefined>(data?.filter((channel) => channel.id == uuid)[0]);
+
+  useEffect(() => {
+    setChannel(data?.filter((channel) => channel.id == uuid)[0]);
+    console.log(data);
+  }, [data]);
 
   console.log(uuid);
-  return <>{uuid}</>;
+  return <>{}</>;
 }
 
 export default MessageList;
