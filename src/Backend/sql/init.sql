4b5c41bc7a279b91750e29778ccf9124bd874769
IrishBruse <Econn50@outlook.com>
2023-12-17 19:00:40 +0000

    Add ryan to db reset

--- src/Backend/sql/init.sql
+++ src/Backend/sql/init.sql
@@ -1,7 +1,6 @@
 -- Insert test users
 INSERT INTO Users (id, username, picture)
-VALUES
-    (
+VALUES (
         "1",
         "Test Account 1",
         "https://picsum.photos/id/10/200"
@@ -10,6 +9,11 @@ VALUES
         "2",
         "Test Account 2",
         "https://picsum.photos/id/20/200"
+    ),
+    (
+        "103472941490344811098",
+        "Ryan Harte",
+        "https://lh3.googleusercontent.com/a/ACg8ocKUnVtPMYSr7hIhnInxRE0VRJjEUHUSWHV_g93oOvHwfw=s96-c"
     ),
     (
         "101915797943822058743",
