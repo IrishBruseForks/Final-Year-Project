cc77ad49bceee1bacacf019e16359a2b8f3344e5
IrishBruse <Econn50@outlook.com>
2024-01-30 01:52:15 +0000

    Fix llm script

--- src/LLM/run.bat
+++ src/LLM/run.bat
@@ -1,10 +1,1 @@
-@REM CD to text generation webui folder
-cd A:\text-generation-webui
-
-@REM get enviroment variables
-
-@echo OFF
-@echo | call A:\text-generation-webui\cmd_windows.bat
-
-@REM run LLM
-python one_click.py --api --nowebui --model ./models/open-llama-3b-v2-wizard-evol-instuct-v2-196k.Q3_K_M.gguf
+a:\text-generation-webui\cmd_windows.bat python one_click.py --api --nowebui --model ./models/open-llama-3b-v2-wizard-evol-instuct-v2-196k.Q3_K_M.gg
